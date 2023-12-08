95567b92843869eef9eae8fdb2e1cd63bca4a35f
Arteandr <artedamatik@gmail.com>
2023-12-08 18:57:24 +0300

    global optimization

--- src/tools.c
+++ src/tools.c
@@ -1,4 +1,4 @@
 #include "../include/tools.h"
 #include <stdlib.h>
 
-int rand_int(int low, int high) { return (rand() % (high - low)) + low; }
+inline int rand_int(int low, int high) { return (rand() % (high - low)) + low; }
